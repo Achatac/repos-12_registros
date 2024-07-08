969594ccb4a202c45862979b059905f5531f3880
Achatac <achatac@unjbg.edu.pe>
2024-07-08 10:31:42 -0500

    declaramos el registro persona

--- registros2.cpp
+++ registros2.cpp
@@ -1,1 +1,14 @@
-estructura persona 
+//estructura persona 
+#include <iostream>
+#include <string>
+using namespace std;
+int main (){
+
+    struct persona {
+        string nombres;
+        long DNI;
+        int edad;
+    }
+    
+    return 0;
+}
