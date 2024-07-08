e8729e1a9ba87bac733c50350ccbb489933ae0da
Achatac <achatac@unjbg.edu.pe>
2024-07-08 09:34:06 -0500

    delaramos los registros

---
+++ registros1.cpp
@@ -0,0 +1,11 @@
+//datos del empleado 
+#include <iostream>
+using namespace std;
+int main (){
+    struct empleado {
+        char nombre[30];
+        char sexo[10];
+        float sueldo[10];
+    };
+    return 0;
+}
