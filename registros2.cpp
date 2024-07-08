c1c43d64c39a60383a75035924bfcde66bde6e1b
Achatac <achatac@unjbg.edu.pe>
2024-07-08 10:50:59 -0500

    anadimos la estructura for y solicitamos los datos de usuario

--- registros2.cpp
+++ registros2.cpp
@@ -8,7 +8,18 @@ int main (){
         string nombres;
         long DNI;
         int edad;
+    };
+
+    int n;
+    cout << "Ingrese la cantidad de personas a registrar: "; cin >> n;
+
+    persona infP[n];
+
+    for (int i = 0; i < n; i++){
+        cout << "Ingrese el nombre de la persona " << i + 1; cin >> infP[i].nombres;
+        cout << "Ingrese el DNI de la persona" << i +1; cin >> infP[i].DNI;
+        cout << "Ingrese la edad de la persona " << i +1; cin >> infP[i].edad
     }
-    
+
     return 0;
 }
