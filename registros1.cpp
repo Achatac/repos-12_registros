bce9ee4728b22166feb8f13d3c5b225beba02315
Achatac <achatac@unjbg.edu.pe>
2024-07-08 09:58:39 -0500

    corregimos algunos errores y modificamos los nombres de la estrucra y el arreglo

--- registros1.cpp
+++ registros1.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 int main (){
     struct empleado {
-        string nombre;
+        string nombres;
         char sexo;
-        float sueldo;
+        double sueldo;
     };
 
+
+    int n;
     cout << "Ingrese la cantidad de empleados: "; cin >> n;
     empleado empleados[n];
     for (int i = 0; i < n; i ++){
-        cout << "Ingrese el nombre del empleado " << i + 1 << " : "; cin >> nombre;
-        empleado.nombre = nombre;
-        cout << "Ingrese el sexo del empleado " << i + 1 << " : "; cin >> sexo;
-        empleado.sexo = sexo;
-        cout << "Ingrese el sueldo del empleado " << i + 1 << " : "; cin >> sueldo;
+        cout << "Ingrese el nombre del empleado " << i + 1 << " : "; cin >> empleados[i].nombres;
+        cout << "Ingrese el sexo del empleado " << i + 1 << " : "; cin >> empleados[i].sexo;
+        cout << "Ingrese el sueldo del empleado " << i + 1 << " : "; cin >> empleados[i].sueldo;
     }
     for (int j = 0; j < n, j++){
         if ()
