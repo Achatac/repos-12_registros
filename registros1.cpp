2747412ab2b9116b17524b012efbf834e5db5c1a
Achatac <achatac@unjbg.edu.pe>
2024-07-08 10:25:22 -0500

    correccion de errores

--- registros1.cpp
+++ registros1.cpp
@@ -3,22 +3,22 @@
 #include <string>
 using namespace std;
 int main (){
+
     struct empleado {
         string nombres;
         char sexo;
         double sueldo;
     };
 
-
-    int n, IMayor, IMenor;
+    int n, IMayor = 0, IMenor = 0;
     cout << "Ingrese la cantidad de empleados: "; cin >> n;
     empleado empleados[n];
     for (int i = 0; i < n; i ++){
         cout << "Ingrese el nombre del empleado " << i + 1 << " : "; cin >> empleados[i].nombres;
-        cout << "Ingrese el sexo del empleado " << i + 1 << " : "; cin >> empleados[i].sexo;
+        cout << "Ingrese el sexo del empleado (m/f)" << i + 1 << " : "; cin >> empleados[i].sexo;
         cout << "Ingrese el sueldo del empleado " << i + 1 << " : "; cin >> empleados[i].sueldo;
     }
-    for (int j = 0; j < n, j++){
+    for (int j = 0; j < n; j++){
         if (empleados[j].sueldo > empleados[IMayor].sueldo){
             IMayor = j;
         }
