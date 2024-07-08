9b32e3de6eec7b550ec7fa0b64c84f031c8cb038
Achatac <achatac@unjbg.edu.pe>
2024-07-08 10:58:48 -0500

    implementamos la estructura for

--- registros2.cpp
+++ registros2.cpp
@@ -18,7 +18,17 @@ int main (){
     for (int i = 0; i < n; i++){
         cout << "Ingrese el nombre de la persona " << i + 1; cin >> infP[i].nombres;
         cout << "Ingrese el DNI de la persona" << i +1; cin >> infP[i].DNI;
-        cout << "Ingrese la edad de la persona " << i +1; cin >> infP[i].edad
+        cout << "Ingrese la edad de la persona " << i +1; cin >> infP[i].edad;
+    }
+
+
+    int sumaE = 0, mayores50 =0 ;
+    float promedio;
+    for (int j = 0; j < n; j ++){
+        sumaE = sumaE + infP[j];
+        if (infP[j].edad > 50){
+            mayores50 ++;
+        }
     }
 
     return 0;
