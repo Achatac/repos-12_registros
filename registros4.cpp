1b992ad84e028c891175e2fb6b7c15eb43ff4a41
Achatac <achatac@unjbg.edu.pe>
2024-07-13 15:40:42 -0500

    anadimos la estructura for para solicitar cada dato

--- registros4.cpp
+++ registros4.cpp
@@ -11,6 +11,18 @@ int main (){
         int NumMedallas
     };
 
+    int n;
+    cout << "Ingrese el numero de atletas: "; cin >> n;
+
+    Atleta Alt[n];
+
+    for (int i; i < n; i ++ ){
+        cout << "Ingrese los datos del atleta #" << i + 1 << endl;
+        cout << "Nombre: " << Alt[i].Nombre;
+        cout << "Pais: " << Alt[i].Pais;
+        cout << "Disciplina" << Alt[i].Disciplina;
+    }
+
 
     return 0;
 }
