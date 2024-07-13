7fcdc3a5ea4a130de7e1174f0720e6307f374267
Achatac <achatac@unjbg.edu.pe>
2024-07-13 14:51:30 -0500

    anadimos la estructura for para solicitar los datos

--- registros3.cpp
+++ registros3.cpp
@@ -16,5 +16,12 @@ int main (){
 
     Persona pers[n];
 
+    for (int i = 0; i < n; i++){
+        cout << "Ingrese los datos de la persona #" << i + 1 << endl;
+        cout << "Nombres: "; cin >> pers[i].Nombre;
+        cout << "Dia de Nacimiento: "; cin >> pers[i].DiaN;
+        cout << "Mes de Nacimiento: "; cin >> pers[i].MesN;
+        cout << "Anio de Nacimiento: "; cin >> pers[i].AnioN;
+    }
     return 0;
 }
