f796e8ce32b8af22a98f3d75655f8b2f1398b895
Achatac <achatac@unjbg.edu.pe>
2024-07-13 15:07:12 -0500

    anadimos las estructuras para solicitar el numero de mes e imprimirlo en la pantalla

--- registros3.cpp
+++ registros3.cpp
@@ -23,5 +23,22 @@ int main (){
         cout << "Mes de Nacimiento: "; cin >> pers[i].MesN;
         cout << "Anio de Nacimiento: "; cin >> pers[i].AnioN;
     }
+
+    int mes;
+    do {
+        cout << "Ingrese un numero de mes: "; cin >> mes;
+        if (mes > 12 || mes < 1){
+            cout << "Las personas que cumplen anios en el mes " << mes << " :";
+            for (int j = 0; j < n; j++ ){
+                if (mes == pers[j].MesN){
+                    cout << "Nombre: " << pers[j].Nombre << ", Dia: " << pers[j].DiaN << ", Mes: " << pers[j].MesN << ", Anio: " << pers[j].Anio.N;
+                }
+            }
+        }
+        else {
+            cout << "valor incorrecto. Intente de nuevo.";
+        }
+    } while (mes != 0);
+
     return 0;
 }
