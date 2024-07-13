b2c4d7633da0d82153777ee142148e6bd730070e
Achatac <achatac@unjbg.edu.pe>
2024-07-13 15:21:02 -0500

    se corregieron errores , se anadio el getline y el cin.ignore para los nombres

--- registros3.cpp
+++ registros3.cpp
@@ -1,9 +1,9 @@
-//repetir mes 
-#include <iostream> 
+#include <iostream>
 #include <string>
-using namespace std;
-int main (){
 
+using namespace std;
+
+int main() {
     struct Persona {
         string Nombre;
         int DiaN;
@@ -12,31 +12,46 @@ int main (){
     };
 
     int n;
-    cout << "Ingrese la cantidad de personas: "; cin >> n;
+    cout << "Ingrese la cantidad de personas: ";
+    cin >> n;
+    cin.ignore();
 
     Persona pers[n];
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cout << "Ingrese los datos de la persona #" << i + 1 << endl;
-        cout << "Nombres: "; cin >> pers[i].Nombre;
-        cout << "Dia de Nacimiento: "; cin >> pers[i].DiaN;
-        cout << "Mes de Nacimiento: "; cin >> pers[i].MesN;
-        cout << "Anio de Nacimiento: "; cin >> pers[i].AnioN;
+        cout << "Nombres: ";
+        getline(cin, pers[i].Nombre);
+        cout << "Dia de Nacimiento: ";
+        cin >> pers[i].DiaN;
+        cout << "Mes de Nacimiento: ";
+        cin >> pers[i].MesN;
+        cout << "Anio de Nacimiento: ";
+        cin >> pers[i].AnioN;
+        cin.ignore();
     }
 
     int mes;
     do {
-        cout << "Ingrese un numero de mes: "; cin >> mes;
-        if (mes > 12 || mes < 1){
-            cout << "Las personas que cumplen anios en el mes " << mes << " :";
-            for (int j = 0; j < n; j++ ){
-                if (mes == pers[j].MesN){
-                    cout << "Nombre: " << pers[j].Nombre << ", Dia: " << pers[j].DiaN << ", Mes: " << pers[j].MesN << ", Anio: " << pers[j].Anio.N;
+        cout << "Ingrese un numero de mes (0 para salir): ";
+        cin >> mes;
+        if (mes == 0) {
+            break;
+        }
+        if (mes >= 1 && mes <= 12) {
+            cout << "Las personas que cumplen anios en el mes " << mes << " :" << endl;
+            bool hayMes = false;
+            for (int j = 0; j < n; j++) {
+                if (mes == pers[j].MesN) {
+                    cout << "Nombre: " << pers[j].Nombre << ", Dia: " << pers[j].DiaN << ", Mes: " << pers[j].MesN << ", Anio: " << pers[j].AnioN << endl;
+                    hayMes = true;
                 }
             }
-        }
-        else {
-            cout << "valor incorrecto. Intente de nuevo.";
+            if (!hayMes) {
+                cout << "No hay personas que cumplan años en este mes." << endl;
+            }
+        } else {
+            cout << "Valor incorrecto. Intente de nuevo." << endl;
         }
     } while (mes != 0);
 
