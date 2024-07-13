209875d4802c1f1e674c68a839299865948fb56c
Achatac <achatac@unjbg.edu.pe>
2024-07-13 14:41:39 -0500

    se anade la libreria string y se declara la estructura Persona

--- registros3.cpp
+++ registros3.cpp
@@ -1,1 +1,14 @@
-repetir mes 
+//repetir mes 
+#include <iostream> 
+#include <string>
+using namespace std;
+int main (){
+
+    struct Persona {
+        string Nombre;
+        int DiaN;
+        int MesN;
+        int AnioN;
+    }
+    return 0;
+}
