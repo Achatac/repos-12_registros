c0b3a5c36e42488ad37a303205d70b62c1667613
Achatac <achatac@unjbg.edu.pe>
2024-07-13 15:35:16 -0500

    declaramos el registro Atleta

--- registros4.cpp
+++ registros4.cpp
@@ -1,1 +1,16 @@
 //gestor de informacion 
+#include <iostream>
+#include <string>
+using namespace std;
+int main (){
+
+    struct Atleta {
+        string Nombre;
+        string Pais;
+        string Disciplina;
+        int NumMedallas
+    };
+
+
+    return 0;
+}
